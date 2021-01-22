c9ea80a190d25c51ea1c43cc081b1d2903013f7a
Jeisson Velasquez <velasquezjeisson@gmail.com>
2021-01-22 15:44:10 -0500

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,8 +6,8 @@
 export const environment = {
   production: true,
   apiUrl: 'http://10.100.22.48:3001/api',
-  apiUrlMat: 'http://10.100.22.48:8081/api',
-  apiUrlMatSignalR: 'http://10.100.22.48:8081',
+  apiUrlMat: 'https://10.100.22.48:5001/api',
+  apiUrlMatSignalR: 'https://10.100.22.48:5001',
   apiUrlNode: 'http://10.100.22.48:1880',
   testUser: {
     token: {},
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,8 +11,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3001/api',
-  apiUrlMat: 'http://localhost:8081/api',
-  apiUrlMatSignalR: 'http://localhost:8081',
+  apiUrlMat: 'https://localhost:5001/api',
+  apiUrlMatSignalR: 'https://localhost:5001',
   apiUrlNode: 'http://10.100.22.48:1880',
   testUser: {
     // tslint:disable

29c305b92eea4f5984ed92d89a33a71ab03d5538
Jeisson Velasquez <velasquezjeisson@gmail.com>
2021-01-22 15:58:28 -0500

    https to http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,8 +6,8 @@
 export const environment = {
   production: true,
   apiUrl: 'http://10.100.22.48:3001/api',
-  apiUrlMat: 'https://10.100.22.48:5001/api',
-  apiUrlMatSignalR: 'https://10.100.22.48:5001',
+  apiUrlMat: 'http://10.100.22.48:5001/api',
+  apiUrlMatSignalR: 'http://10.100.22.48:5001',
   apiUrlNode: 'http://10.100.22.48:1880',
   testUser: {
     token: {},
